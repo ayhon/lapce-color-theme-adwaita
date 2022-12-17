a5ca322383d572f9153cf561787c1a85039a4037
ayhon <43295942+ayhon@users.noreply.github.com>
2022-12-17 14:34:30 +0100

    Initial release

---
+++ colors.py
@@ -0,0 +1,327 @@
+colorscheme = {
+	# Accent colors
+	"accent_color"    :          { "light": "#1c71d8", "dark": "#78aeed" },
+	"accent_bg_color" :          { "light": "#3584e4", "dark": "#3584e4" },
+	"accent_fg_color" :          { "light": "#ffffff", "dark": "#ffffff" },
+
+	# Destructive colors
+	"destructive_color"	 :     { "light": "#c01c28", "dark": "#ff7b63" },
+	"destructive_bg_color" :     { "light": "#e01b24", "dark": "#c01c28" },
+	"destructive_fg_color" :     { "light": "#ffffff", "dark": "#ffffff" },
+
+	# Success colors
+	"success_color"	 :         { "light": "#26a269", "dark": "#8ff0a4" },
+	"success_bg_color" :         { "light": "#2ec27e", "dark": "#26a269" },
+	"success_fg_color" :         { "light": "#ffffff", "dark": "#ffffff" },
+
+	# Warning colors
+	"warning_color"	:          { "light": "#ae7b03", "dark": "#f8e45c" },
+	"warning_bg_color"	:      { "light": "#e5a50a", "dark": "#cd9309" },
+	"warning_fg_color"	:      { "light": "#000000cc", "dark": "#000000cc" },
+
+	# Error colors
+	"error_color"		:          { "light": "#c01c28", "dark": "#ff7b63" },
+	"error_bg_color"	:          { "light": "#e01b24", "dark": "#c01c28" },
+	"error_fg_color"	:          { "light": "#ffffff", "dark": "#ffffff" },
+
+	# Window colors
+	"window_bg_color"		:      { "light": "#fafafa", "dark": "#242424" },
+	"window_fg_color"		:      { "light": "#000000cc", "dark": "#ffffff" },
+
+	# View colors
+	"view_bg_color"	:          { "light": "#ffffff", "dark": "#1e1e1e" },
+	"view_fg_color"	:          { "light": "#000000cc", "dark": "#ffffff" },
+
+	# Header bar colors
+	"headerbar_bg_color" :      { "light": "#ebebeb", "dark": "#303030" },
+	"headerbar_fg_color" :      { "light": "#000000cc", "dark": "#ffffff" },
+	"headerbar_border_color"	:  { "light": "#000000cc", "dark": "#ffffff" },
+	"headerbar_backdrop_color" : { "light": "#fafafa", "dark": "#242424" },
+	"headerbar_shade_color" :  { "light": "#00000011", "dark": "#0000005b" },
+
+	# Card colors
+	"card_bg_color"	:            { "light": "#ffffff", "dark": "ffffff14" },
+	"card_fg_color"	:            { "light": "#000000cc", "dark": "#ffffff" },
+	"card_shade_color"	:         { "light": "#00000011", "dark": "#0000005b" },
+
+	# Dialog colors
+	"dialog_bg_color"		:         { "light": "#fafafa", "dark": "#383838" },
+	"dialog_fg_color"		:         { "light": "#000000cc", "dark": "#ffffff" },
+
+	# Popover colors
+	"popover_bg_color"	:         { "light": "#ffffff", "dark": "#383838" },
+	"popover_fg_color"	:         { "light": "#000000cc", "dark": "#ffffff" },
+
+	# Miscellaneous colors
+	"shade_color"		:             { "light": "#00000011", "dark": "#0000005b" },
+	"scrollbar_outline_color"		: { "light": "#ffffff", "dark": "#0000007f" },
+
+	# Palette colors
+	"blue_1" :                   { "light":	"#99c1f1", "dark": "#99c1f1" },
+	"blue_2" :                   { "light":	"#62a0ea", "dark": "#62a0ea" },
+	"blue_3" :                   { "light":	"#3584e4", "dark": "#3584e4" },
+	"blue_4" :                   { "light":	"#1c71d8", "dark": "#1c71d8" },
+	"blue_5" :                   { "light":	"#1a5fb4", "dark": "#1a5fb4" },
+	"green_1" :                  { "light":	"#8ff0a4", "dark": "#8ff0a4" },
+	"green_2" :                  { "light":	"#57e389", "dark": "#57e389" },
+	"green_3" :                  { "light":	"#33d17a", "dark": "#33d17a" },
+	"green_4" :                  { "light":	"#2ec27e", "dark": "#2ec27e" },
+	"green_5" :                  { "light":	"#26a269", "dark": "#26a269" },
+	"yellow_1" :                 { "light":	"#f9f06b", "dark": "#f9f06b" },
+	"yellow_2" :                 { "light":	"#f8e45c", "dark": "#f8e45c" },
+	"yellow_3" :                 { "light":	"#f6d32d", "dark": "#f6d32d" },
+	"yellow_4" :                 { "light":	"#f5c211", "dark": "#f5c211" },
+	"yellow_5" :                 { "light":	"#e5a50a", "dark": "#e5a50a" },
+	"orange_1" :                 { "light":	"#ffbe6f", "dark": "#ffbe6f" },
+	"orange_2" :                 { "light":	"#ffa348", "dark": "#ffa348" },
+	"orange_3" :                 { "light":	"#ff7800", "dark": "#ff7800" },
+	"orange_4" :                 { "light":	"#e66100", "dark": "#e66100" },
+	"orange_5" :                 { "light":	"#c64600", "dark": "#c64600" },
+	"red_1" :                    { "light":	"#f66151", "dark": "#f66151" },
+	"red_2" :                    { "light":	"#ed333b", "dark": "#ed333b" },
+	"red_3" :                    { "light":	"#e01b24", "dark": "#e01b24" },
+	"red_4" :                    { "light":	"#c01c28", "dark": "#c01c28" },
+	"red_5" :                    { "light":	"#a51d2d", "dark": "#a51d2d" },
+	"purple_1" :                 { "light":	"#dc8add", "dark": "#dc8add" },
+	"purple_2" :                 { "light":	"#c061cb", "dark": "#c061cb" },
+	"purple_3" :                 { "light":	"#9141ac", "dark": "#9141ac" },
+	"purple_4" :                 { "light":	"#813d9c", "dark": "#813d9c" },
+	"purple_5" :                 { "light":	"#613583", "dark": "#613583" },
+	"brown_1" :                  { "light":	"#cdab8f", "dark": "#cdab8f" },
+	"brown_2" :                  { "light":	"#b5835a", "dark": "#b5835a" },
+	"brown_3" :                  { "light":	"#986a44", "dark": "#986a44" },
+	"brown_4" :                  { "light":	"#865e3c", "dark": "#865e3c" },
+	"brown_5" :                  { "light":	"#63452c", "dark": "#63452c" },
+	"light_1" :                  { "light":	"#ffffff", "dark": "#ffffff" },
+	"light_2" :                  { "light":	"#f6f5f4", "dark": "#f6f5f4" },
+	"light_3" :                  { "light":	"#deddda", "dark": "#deddda" },
+	"light_4" :                  { "light":	"#c0bfbc", "dark": "#c0bfbc" },
+	"light_5" :                  { "light":	"#9a9996", "dark": "#9a9996" },
+	"dark_1" :                   { "light":	"#77767b", "dark": "#77767b" },
+	"dark_2" :                   { "light":	"#5e5c64", "dark": "#5e5c64" },
+	"dark_3" :                   { "light":	"#3d3846", "dark": "#3d3846" },
+	"dark_4" :                   { "light":	"#241f31", "dark": "#241f31" },
+	"dark_5" :                   { "light":	"#000000", "dark": "#000000" },
+}
+
+syntax = {
+    "comment"              : {'dark': "#5C6773",  'light': "#ABB0B6"},
+    "constant"             : {'dark': "#FFEE99",  'light': "#A37ACC"},
+    "type"                 : {'dark': "#E7C547",  'light': "#E7C547"},
+    "typeAlias"            : {'dark': "#E7C547",  'light': "#E7C547"},
+    "number"               : {'dark': "#E7C547",  'light': "#E7C547"},
+    "enum"                 : {'dark': "#36A3D9",  'light': "#36A3D9"},
+    "struct"               : {'dark': "#36A3D9",  'light': "#36A3D9"},
+    "structure"            : {'dark': "#36A3D9",  'light': "#36A3D9"},
+    "interface"            : {'dark': "#36A3D9",  'light': "#36A3D9"},
+    "attribute"            : {'dark': "#E7C547",  'light': "#E7C547"},
+    "constructor"          : {'dark': "#E7C547",  'light': "#E7C547"},
+    "function"             : {'dark': "#FFB454",  'light': "#F29718"},
+    "method"               : {'dark': "#E7C547",  'light': "#E7C547"},
+    "function.method"      : {'dark': "#36A3D9",  'light': "#36A3D9"},
+    "keyword"              : {'dark': "#FF7733",  'light': "#FF7733"},
+    "selfKeyword"          : {'dark': "#FFEE99",  'light': "#A37ACC"},
+    "field"                : {'dark': "#F07178",  'light': "#F07178"},
+    "property"             : {'dark': "#E6B673",  'light': "#E6B673"},
+    "enumMember"           : {'dark': "#F07178",  'light': "#F07178"},
+    "enum-member"          : {'dark': "#F07178",  'light': "#F07178"},
+    "string"               : {'dark': "#B8CC52",  'light': "#86B300"},
+    "builtinType"          : {'dark': "#E6B673",  'light': "#E6B673"},
+    "escape"               : {'dark': "#95E6CB",  'light': "#4CBF99"},
+    "string.escape"        : {'dark': "#B8CC52",  'light': "#86B300"},
+    "embedded"             : {'dark': "#95E6CB",  'light': "#4CBF99"},
+    "variable.other.member": {'dark': "#F07178",  'light': "#F07178"}
+}
+
+# Defining vairables in the `color-themes.base` section of the {theme}.toml file should
+# work. Perhaps consider adding this as a feature and PR
+#
+#   fn resolve_color(
+#       colors: &IndexMap<String, String>,
+#       base: &ThemeBaseColor,
+#       default: Option<&HashMap<String, Color>>,
+#   ) -> HashMap<String, Color> {
+#       colors
+#           .iter()
+#           .map(|(name, hex)| {
+#               let color = if let Some(stripped) = hex.strip_prefix('$') {
+#                   base.get(stripped).cloned()
+#               } else {
+#                   Color::from_hex_str(hex).ok()
+#               };
+#   
+#               let color = color
+#                   .or_else(|| {
+#                       default.and_then(|default| default.get(name).cloned())
+#                   })
+#                   .unwrap_or(Color::rgb8(0, 0, 0));
+#   
+#               (name.to_string(), color)
+#           })
+#           .collect()
+#   }
+# Colors from https://github.com/GNOME/libadwaita/blob/d36716d035392f1d8a36da91044385c44d47d965/doc/named-colors.md
+
+for variant in ["light","dark"]:
+    def color(name, dark = None):
+        if dark and variant == 'dark':
+            return colorscheme[dark][variant]
+        return colorscheme[name][variant]
+    def synt(name):
+        return syntax[name][variant]
+
+    contents = f"""
+# This theme was generated automagically with a python script. For any official
+# modifications of the theme, please modify the python file
+
+[color-theme]
+name                              = "Adwaita {variant}"
+
+[color-theme.base]
+white                             = "{color('light_4','light_2')}"
+black                             = "{color('dark_4','dark_2')}"
+grey                              = "{color('dark_1','light_5')}"
+blue                              = "{color('blue_4','blue_2')}"
+red                               = "{color('red_4','red_2')}"
+yellow                            = "{color('yellow_4','yellow_2')}"
+orange                            = "{color('orange_4','orange_2')}"
+green                             = "{color('green_4','green_2')}"
+purple                            = "{color('purple_4','purple_2')}"
+cyan                              = "#56B6C2" # Unchanged
+magenta                           = "#C678DD" # Unchanged
+
+[color-theme.syntax]
+"comment"                         = "{synt('comment')}"
+"constant"                        = "{synt('constant')}"
+"type"                            = "{synt('type')}"
+"typeAlias"                       = "{synt('typeAlias')}"
+"number"                          = "{synt('number')}"
+"enum"                            = "{synt('enum')}"
+"struct"                          = "{synt('struct')}"
+"structure"                       = "{synt('structure')}"
+"interface"                       = "{synt('interface')}"
+"attribute"                       = "{synt('attribute')}"
+"constructor"                     = "{synt('constructor')}"
+"function"                        = "{synt('function')}"
+"method"                          = "{synt('method')}"
+"function.method"                 = "{synt('function.method')}"
+"keyword"                         = "{synt('keyword')}"
+"selfKeyword"                     = "{synt('selfKeyword')}"
+"field"                           = "{synt('field')}"
+"property"                        = "{synt('property')}"
+"enumMember"                      = "{synt('enumMember')}"
+"enum-member"                     = "{synt('enum-member')}"
+"string"                          = "{synt('string')}"
+"builtinType"                     = "{synt('builtinType')}"
+"escape"                          = "{synt('escape')}"
+"string.escape"                   = "{synt('string.escape')}"
+"embedded"                        = "{synt('embedded')}"
+"variable.other.member"           = "{synt('variable.other.member')}"
+
+[color-theme.ui]
+"activity.background"             = "{color("red_2")}" # Unknown
+"activity.current"                = "{color('accent_color')}"
+
+"completion.background"           = "{color("view_bg_color")}"
+"completion.current"              = "{color("window_bg_color")}"
+
+"editor.background"               = "{color("view_bg_color")}" #"#242936"
+"editor.foreground"               = "{color('view_fg_color')}"
+"editor.caret"                    = "{color("accent_color")}"
+"editor.current_line"             = "{color("headerbar_bg_color")}"
+"editor.dim"                      = "{color('dark_2','light_4')}" # "#5C6370" # Don't know what it does yet
+"editor.drag_drop_background"     = "{color('accent_bg_color')}55"
+"editor.drag_drop_tab_background" = "{color('accent_bg_color')}55"
+"editor.sticky_header_background" = "{color('window_bg_color')}"
+"editor.focus"                    = "{color('window_bg_color')}" # "#CCCCCC"
+"editor.indent_guide"             = "{color('card_shade_color')}"
+"editor.link"                     = "{color('accent_color')}"
+"editor.selection"                = "{color('accent_bg_color')}40"
+"editor.visible_whitespace"       = "{color('window_bg_color')}" # "#5C6370"
+
+"error_lens.error.background"     = "{color("error_bg_color")}80"
+"error_lens.error.foreground"     = "{color("error_fg_color")}"
+
+"error_lens.other.background"     = "#5C637020"
+"error_lens.other.foreground"     = "#5C6370"
+
+"error_lens.warning.background"   = "{color('warning_bg_color')}80"
+"error_lens.warning.foreground"   = "{color('warning_fg_color')}"
+
+"hover.background"                = "{color("view_bg_color")}"
+
+"inlay_hint.background"           = "{color('popover_bg_color')}80"
+"inlay_hint.foreground"           = "{color('popover_fg_color')}"
+
+"lapce.border"                    = "{color("card_shade_color")}"
+"lapce.button.primary.background" = "{color("accent_bg_color")}"
+"lapce.button.primary.foreground" = "{color("accent_fg_color")}"
+"lapce.dropdown_shadow"           = "{color('shade_color')}"
+"lapce.error"                     = "{color('error_color')}"
+"lapce.icon.active"               = "{color('accent_color')}"
+"lapce.icon.inactive"             = "{color('window_fg_color')}"
+"lapce.plugin.author"             = "{color('window_fg_color')}"
+"lapce.plugin.description"        = "{color('window_fg_color')}"
+"lapce.plugin.name"               = "{color('window_fg_color')}"
+"lapce.remote.connected"          = "{color('success_color')}"
+"lapce.remote.connecting"         = "{color('warning_color')}"
+"lapce.remote.disconnected"       = "{color('destructive_color')}"
+"lapce.remote.local"              = "{color('window_bg_color')}"
+"lapce.scroll_bar"                = "{color('dialog_bg_color')}"
+"lapce.tab.inactive.background"   = "{color('window_bg_color')}"
+"lapce.tab.inactive.foreground"   = "{color('window_fg_color')}"
+"lapce.tab.inactive.underline"    = "{color('card_shade_color')}"
+"lapce.tab.active.background"     = "{color('accent_bg_color')}20"
+"lapce.tab.active.foreground"     = "{color('window_fg_color')}"
+"lapce.tab.active.underline"      = "{color('accent_color')}"
+"lapce.tab.separator"             = "{color('card_shade_color')}"
+"lapce.warn"                      = "{color('warning_color')}"
+
+"markdown.blockquote"             = "#898989"
+
+"palette.background"              = "{color('popover_bg_color')}"
+"palette.foreground"              = "{color('popover_fg_color')}"
+"palette.current.background"      = "{color('accent_bg_color')}20"
+"palette.current.foreground"      = "{color('popover_fg_color')}"
+
+"panel.background"                = "{color('view_bg_color')}"
+"panel.current.background"        = "{color('accent_bg_color')}40"
+"panel.current.foreground"        = "{color('accent_fg_color')}"
+"panel.hovered.background"        = "{color('accent_bg_color')}21"
+"panel.hovered.foreground"        = "{color('accent_fg_color')}"
+"panel.foreground"                = "{color('view_fg_color')}"
+
+"source_control.added"            = "{color('green_4','green_2')}32"
+"source_control.modified"         = "{color('blue_4','blue_2')}32"
+"source_control.removed"          = "{color('red_4','red_2')}32"
+
+"status.background"               = "{color('view_bg_color')}"
+"status.foreground"               = "{color('view_fg_color')}"
+
+"status.modal.insert"             = "$red"
+"status.modal.normal"             = "$blue"
+"status.modal.terminal"           = "$purple"
+"status.modal.visual"             = "$yellow"
+
+"terminal.background"             = "{color('window_bg_color')}"
+"terminal.black"                  = "$black"
+"terminal.blue"                   = "$blue"
+"terminal.bright_black"           = "#545862"
+"terminal.bright_blue"            = "$blue"
+"terminal.bright_cyan"            = "$cyan"
+"terminal.bright_green"           = "$green"
+"terminal.bright_magenta"         = "$magenta"
+"terminal.bright_red"             = "$red"
+"terminal.bright_white"           = "#C8CCD4"
+"terminal.bright_yellow"          = "$yellow"
+"terminal.cursor"                 = "$white"
+"terminal.cyan"                   = "$cyan"
+"terminal.foreground"             = "$white"
+"terminal.green"                  = "$green"
+"terminal.magenta"                = "$magenta"
+"terminal.red"                    = "$red"
+"terminal.white"                  = "$white"
+"terminal.yellow"                 = "$yellow"
+"""
+    with open(f"adwaita-{variant}.toml","w") as f:
+        f.write(contents)
